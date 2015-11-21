4811fb94cd44fb5ca410f1ef6e42e9cb5199aa93
Aditya Kresna permana <slaveofcode@gmail.com>
2015-11-21 12:09:08 +0700

    Prepare for publish

---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+setup(
+  name='pycrawler',
+  packages=['pycrawler'],  # must be filled with your python directories
+  version='1.0.0',
+  description='A Python crawler tool to grab page(s) information from their html data',
+  author='Aditya Kresna Permana',
+  author_email='zeandcode@gmail.com',
+  url='https://github.com/slaveofcode/pycrawler',
+  download_url='https://github.com/slaveofcode/pycrawler/archive/v1.0.0.zip',
+  keywords=[
+      'crawler',
+      'pycrawler',
+      'boilerpipe',
+      'boilerpipe3',
+      'page crawler',
+      'html crawler',
+      'document crawler'
+  ],
+  classifiers=[],
+)
