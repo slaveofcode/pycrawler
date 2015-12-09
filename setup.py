e014a4e69a6045ac2ccc9e0d4fee87f5719646a3
Aditya Kresna permana <slaveofcode@gmail.com>
2015-12-09 23:05:20 +0700

    update version

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from setuptools import setup
 setup(
   name='PyCrawler3',
   packages=['pycrawler'],  # must be filled with your python directories
-  version='1.1.2',
+  version='1.2.0',
   description='A Python crawler tool to grab page(s) information from html data. This only support Python 3',
   author='Aditya Kresna Permana',
   author_email='zeandcode@gmail.com',
   url='https://github.com/slaveofcode/pycrawler',
-  download_url='https://github.com/slaveofcode/pycrawler/archive/v1.1.2.zip',
+  download_url='https://github.com/slaveofcode/pycrawler/archive/v1.2.0.zip',
   install_requires=[
       'JPype1==0.6.1',
       'JPype1-py3==0.5.5.2',
