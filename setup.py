ee8513c6340851f33e1c19a3fec2c7ba65773388
Aditya Kresna permana <slaveofcode@gmail.com>
2015-12-09 17:58:56 +0700

    Added dependencies into installation with setup.py

--- setup.py
+++ setup.py
@@ -2,12 +2,20 @@ from setuptools import setup
 setup(
   name='PyCrawler3',
   packages=['pycrawler'],  # must be filled with your python directories
-  version='1.1.1',
+  version='1.1.2',
   description='A Python crawler tool to grab page(s) information from html data. This only support Python 3',
   author='Aditya Kresna Permana',
   author_email='zeandcode@gmail.com',
   url='https://github.com/slaveofcode/pycrawler',
-  download_url='https://github.com/slaveofcode/pycrawler/archive/v1.1.1.zip',
+  download_url='https://github.com/slaveofcode/pycrawler/archive/v1.1.2.zip',
+  install_requires=[
+      'JPype1==0.6.1',
+      'JPype1-py3==0.5.5.2',
+      'beautifulsoup4==4.4.1',
+      'boilerpipe-py3==1.2.0.0',
+      'charade==1.0.3',
+      'requests==2.8.1'
+  ],
   keywords=[
       'crawler',
       'pycrawler',
